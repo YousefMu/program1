af9e1ba96299887a8a3236dbe76dbee54a47fd03
Yousef Alasmar <Your github email>
2021-10-13 13:37:07 +0300

    update some tasks

--- ToDoList.playground/Contents.swift
+++ ToDoList.playground/Contents.swift
@@ -1,8 +1,12 @@
+enum Status {
+    case Complete
+    case notComplete
+}
+
 struct toDoList {
-    var name : String
-    var age : Int
+    var task : String
     var date : Any
-    var isComplete : Bool
+    var status : Status
     
 }
 
@@ -22,9 +26,9 @@ class List {
     func edit(index : Int , elemnt : String) {
         // if for error handling
         if (index <= lst2.count - 1){
-        lst2 [index].name = elemnt
+        lst2 [index].task = elemnt
     } else {
-        print("the name not found")
+        print("the task not found")
     }
         // function to remove value .
     }
@@ -39,27 +43,26 @@ class List {
     
     func display() {
         for i in lst2 {
-            print("Name : \(i.name), Age : \(i.age), Date : \(i.date)")
+            print("Task : \(i.task), Date : \(i.date), Status : \(i.status)")
         }
     
     }
     }
 
-    
-var test1 = toDoList(name: "Yousef", age: 28, date: "16/11/1994", isComplete: false)
-var test2 = toDoList(name: "mohammad", age: 17, date: "20/10/2005", isComplete: false)
+var test1 = toDoList(task: "Home Work",  date: "16/11/2020", status: .notComplete )
+var test2 = toDoList(task: "Fix The Car",  date: "20/10/2021", status: .Complete)
 
 var lst1 = List()
 lst1.add2(item: test1)
 lst1.add2(item: test2)
 
-lst1.add2(item: toDoList(name: "yazan", age: 90, date: "20/10/2005", isComplete: false))
-lst1.add2(item: toDoList(name: "abdullah", age: 199, date: "20/10/2005", isComplete: false))
+lst1.add2(item: toDoList(task: "Drink Coffee",  date: "20/10/2019", status:.Complete ))
+lst1.add2(item: toDoList(task: "Sleep ",  date: "20/10/2017", status:.notComplete ))
 
 
 //print(lst1.lst2[3])
-lst1.edit(index: 1, elemnt: "naser")
-lst1.remov(index: 0)
+lst1.edit(index: 7, elemnt: "Meeting")
+lst1.remov(index: 6)
 //print(test1)
 lst1.display()
 
