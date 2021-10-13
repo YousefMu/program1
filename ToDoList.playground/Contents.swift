64656b6d284762487c674c04aa5c22a88a794072
Yousef Alasmar <Your github email>
2021-10-13 11:22:26 +0300

    add the error handling

--- ToDoList.playground/Contents.swift
+++ ToDoList.playground/Contents.swift
@@ -20,24 +20,32 @@ class List {
         // function to update value .
     }
     func edit(index : Int , elemnt : String) {
+        // if for error handling
+        if (index <= lst2.count - 1){
         lst2 [index].name = elemnt
-        
+    } else {
+        print("the name not found")
+    }
         // function to remove value .
     }
     func remov(index :Int) {
+        // if for error handling
+        if (index <= lst2.count - 1){
         lst2.remove(at: index)
-        
+        } else {
+            print("Cannot find item")
+        }}
         // function to display all values .
-    }
+    
     func display() {
         for i in lst2 {
-            print(i.self)
+            print("Name : \(i.name), Age : \(i.age), Date : \(i.date)")
         }
-        
-    }
     
-}
+    }
+    }
 
+    
 var test1 = toDoList(name: "Yousef", age: 28, date: "16/11/1994", isComplete: false)
 var test2 = toDoList(name: "mohammad", age: 17, date: "20/10/2005", isComplete: false)
 
@@ -54,3 +62,4 @@ lst1.edit(index: 1, elemnt: "naser")
 lst1.remov(index: 0)
 //print(test1)
 lst1.display()
+
